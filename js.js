92b487b229485fcfed65596a264e0f59243fec2c
loren <practicostrabajos416@gmail.com>
2022-01-24 11:54:05 -0300

    clase 6 js

---
+++ js.js
@@ -0,0 +1,35 @@
+//for
+
+let empleados =["juan","roberto","maria","ana","ayelen","carlos","federico","ignacio","ramiro","nicolas"]
+
+//console.log("cantidad de empleados" +  empleados.length);
+
+let mensaje="";
+
+
+for (let i = 0; i < empleados.length; i++) {
+   console.log(empleados[i]);
+
+}
+
+console.log("---------------------------------------------");
+
+
+let ventas=[2500,150,300,1200,2800,4500]
+
+for (let i = 0; i < ventas.length; i++) {
+ console.log(ventas[i]);
+    
+}
+
+console.log("-------------------------------------------------");
+
+
+//imprimimos la lista de empleados en la vista
+
+
+for (let i = 0; i < empleados.length; i++) {
+   mensaje = empleados[i];
+   document.getElementById('caja').innerHTML = mensaje
+    
+}
