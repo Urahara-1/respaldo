d2a56a7050dac7d48ba3b2af9968a5b29cbaf0b5
loren <practicostrabajos416@gmail.com>
2022-01-25 14:47:16 -0300

    clase 6 js

--- js.js
+++ js.js
@@ -5,7 +5,8 @@ let empleados =["juan","roberto","maria","ana","ayelen","carlos","federico","ign
 //console.log("cantidad de empleados"+  +  empleados.length);
 
 let mensaje="";
-
+let mensaje1="";
+let mensaje2="";
 
 for (let i = 0; i < empleados.length; i++) {
    console.log(empleados[i]);
@@ -33,3 +34,26 @@ for (let i = 0; i < empleados.length; i++) {
    document.getElementById('caja').innerHTML = mensaje
     
 }
+
+console.log("-------------------------------------------------");
+
+//imprimir los empelados mediante bucle while
+ let i = 0;
+while (i< empleados.length) {
+   mensaje1 += empleados[i] +'<br>';
+   document.getElementById('caja1').innerHTML = mensaje
+   ++i
+}
+
+//funciones
+
+ function imprimir_empleados() {
+   for (let i = 0; i < empleados.length; i++) {
+      mensaje2 += empleados[i] +'<br>';
+      document.getElementById('caja2').innerHTML = mensaje2
+
+ }
+}
+
+//llamo a la funcion para que cumpla su trabajo 
+imprimir_empleados();
